8fc6d4f5b72a45c9071f8be95056267bff4fe946
tykita <4rchive7@gmail.com>
2020-10-01 08:24:59 +0900

    Basic setting is done
    exit
    ''
    :

---
+++ src/main/java/com/toyproject/springboot/controller/SpringbootController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SpringbootController {
+
+    public static String getMethodName() {
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/respTest")
+    public String respTest(){
+        return "test..l";
+    }
+
+
+    @RequestMapping(value="/mappingHtmlTest")
+    public String mappingHtml(){
+        System.out.println(String.format("[%s] is called on server", getMethodName()));
+        System.out.println(String.format("[%s] is closed on server", getMethodName()));
+        return "mappingHtml.html";
+    }
+
+}
